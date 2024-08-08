0df58153f92a98ab14a6e1b25df4650a31d67974
jbyer <byerjamie@gmail.com>
2024-08-08 10:31:43 -0600

    initial commit

---
+++ process_data.py
@@ -0,0 +1,8 @@
+
+
+# read in data
+
+# will loop over all files...
+filename = "453024025.0001.2024.06.06.18.04.52.000.E.miniseed"
+path = Path("/gilbert_lab/Whitehorse_ANT/")
+path = Path("/gilbert_lab/Whitehorse_ANT/FDSN_Information.xml")
